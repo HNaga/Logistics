54475cb504f3c5ff1b01d90aa170f1a8c44166cc
hnaga <h.abulnaga@gmail.com>
2018-04-18 14:27:41 +0200

    setup.py update

--- setup.py
+++ setup.py
@@ -14,15 +14,6 @@ with open('logistics/__init__.py', 'rb') as f:
 	version = str(ast.literal_eval(_version_re.search(
 		f.read().decode('utf-8')).group(1)))
 
-from pip.req import parse_requirements
-import re, ast
-
-# get version from __version__ variable in shipping_management/__init__.py
-_version_re = re.compile(r'__version__\s+=\s+(.*)')
-
-with open('shipping_management/__init__.py', 'rb') as f:
-    version = str(ast.literal_eval(_version_re.search(
-        f.read().decode('utf-8')).group(1)))
 
 
 requirements = parse_requirements("requirements.txt", session="")

ff579266bd4a2b905e789b999ab37bb050221a8b
hnaga <h.abulnaga@gmail.com>
2018-05-06 08:44:27 +0200

    page

---
+++ logistics/logistics/page/myth_trade/myth_trade.js
@@ -0,0 +1,54 @@
+#import { Chart } from "frappe-charts"
+
+frappe.pages['myth-trade'].on_page_load = function(wrapper) {
+	var page = frappe.ui.make_app_page({
+		parent: wrapper,
+		title: 'MYTH Trade',
+		single_column: true
+	});
+console.log(page)
+page.add_menu_item('projects',() => frappe.set_route('List','Project'))
+
+ 
+let chart = new frappe.Chart( "#chart", { // or DOM element
+    data: {
+      labels: ["12am-3am", "3am-6am", "6am-9am", "9am-12pm",
+      "12pm-3pm", "3pm-6pm", "6pm-9pm", "9pm-12am"],
+
+      datasets: [
+        {
+          name: "Some Data", chartType: 'bar',
+          values: [25, 40, 30, 35, 8, 52, 17, -4]
+        },
+        {
+          name: "Another Set", chartType: 'bar',
+          values: [25, 50, -10, 15, 18, 32, 27, 14]
+        },
+        {
+          name: "Yet Another", chartType: 'line',
+          values: [15, 20, -3, -15, 58, 12, -17, 37]
+        }
+      ],
+
+      yMarkers: [{ label: "Marker", value: 70,
+        options: { labelPos: 'left' }}],
+      yRegions: [{ label: "Region", start: -10, end: 50,
+        options: { labelPos: 'right' }}]
+    },
+
+    title: "My Awesome Chart",
+    type: 'axis-mixed', // or 'bar', 'line', 'pie', 'percentage'
+    height: 300,
+    colors: ['purple', '#ffa3ef', 'light-blue'],
+
+    tooltipOptions: {
+      formatTooltipX: d => (d + '').toUpperCase(),
+      formatTooltipY: d => d + ' pts',
+    }
+  });
+
+  chart.export();
+
+
+
+}
